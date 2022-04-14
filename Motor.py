722744482c2ba22cfb8d94a806155cd2266e4198
Jeffrey Nelson <jnelson@sunriselabs.com>
2022-04-14 17:30:05 -0400

    add motor driver for stepper hat

--- Motor.py
+++ Motor.py
@@ -5,19 +5,19 @@ class Motor:
     def __init__(self):
         self.motor1 = DRV8825(dir_pin=13, step_pin=19,
                               enable_pin=12, mode_pins=(16, 17, 20))
-        self.motor1.SetMicroStep('softward', 'fullstep')
+        self.motor1.SetMicroStep('softward', '1/32step')
 
     def move_monochrom_backward(self, number_of_steps, disable=True):
         steps = int(number_of_steps)
         self.motor1.TurnStep(
-            Dir='forward', steps=steps, stepdelay=0.00005)
+            Dir='forward', steps=steps)
         if disable:
             self.motor1.Stop()
 
     def move_monochrom_forward(self, number_of_steps, disable=True):
         steps = int(number_of_steps)
         self.motor1.TurnStep(
-            Dir='backward', steps=steps, stepdelay=0.00005)
+            Dir='backward', steps=steps)
         if disable:
             self.motor1.Stop()
 
