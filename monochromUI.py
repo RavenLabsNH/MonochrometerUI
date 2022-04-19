2dce04b2931f02802291e844e7795ab9d4a58b2e
Jeffrey Nelson <jnelson@sunriselabs.com>
2022-04-19 17:50:50 -0400

    stop button

--- monochromUI.py
+++ monochromUI.py
@@ -11,18 +11,19 @@ class MonochromUI():
         self.index = 0
 
     def create_page(self):
-        with dpg.handler_registry():
-            dpg.add_mouse_down_handler(callback=self.move_monochrom)
-            dpg.add_mouse_release_handler(callback=self.stop_mnonochrom)
+        # with dpg.handler_registry():
+        #     dpg.add_mouse_down_handler(callback=self.move_monochrom)
+        #     dpg.add_mouse_release_handler(callback=self.stop_mnonochrom)
 
         with dpg.window(tag="Monochrom", width=800, height=400) as window:
-            dpg.add_button(label="<", width=202, height=40, tag="left_button")
+            dpg.add_button(label="<", width=202, height=40, tag="left_button", callback=self.move_monochrom)
             dpg.add_button(label=">", width=202, height=40, tag="right_button")
+            dpg.add_button(label="Stop", width=202, height=40, tag="right_button", callback=self.stop_mnonochrom)
 
         dpg.create_viewport(title='Monochrom', width=800, height=400, x_pos=40, y_pos=40)
         dpg.setup_dearpygui()
         dpg.set_viewport_vsync(True)
-        #dpg.configure_app(wait_for_input=True)
+        dpg.configure_app(wait_for_input=True)
 
         #dpg.show_metrics()
         dpg.show_viewport()
@@ -39,7 +40,7 @@ class MonochromUI():
                 dpg.render_dearpygui_frame()
 
     def move_monochrom(self):
-        if dpg.is_item_active("left_button") and self.running_flag.value is False:
+        if self.running_flag.value is False:
             self.running_flag.value = True
             forward_process = mp.Process(target=self.motor.move_monochrom_backward_continuous)
             forward_process.start()
