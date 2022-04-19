611358e2e428fbdf4a258680b4022005c51cecbf
Jeffrey Nelson <jnelson@sunriselabs.com>
2022-04-19 19:31:19 -0400

    stop button

--- monochromUI.py
+++ monochromUI.py
@@ -12,9 +12,9 @@ class MonochromUI():
         self.index = 0
 
     def create_page(self):
-        # with dpg.handler_registry():
-        #     dpg.add_mouse_down_handler(callback=self.move_monochrom)
-        #     dpg.add_mouse_release_handler(callback=self.stop_mnonochrom)
+        with dpg.handler_registry():
+            dpg.add_mouse_down_handler(callback=self.move_monochrom)
+            dpg.add_mouse_release_handler(callback=self.stop_mnonochrom)
 
         with dpg.window(tag="Monochrom", width=800, height=400) as window:
             dpg.add_button(label="<", width=202, height=40, tag="left_button", callback=self.move_monochrom)
@@ -41,16 +41,20 @@ class MonochromUI():
                 dpg.render_dearpygui_frame()
 
     def move_monochrom(self):
-        if self.running_flag.value is False:
+        if dpg.is_item_active("left_button") and self.running_flag.value is False:
             self.running_flag.value = True
-            forward_process = mp.Process(target=self.move_process)
+            backward_process = mp.Process(target=self.move_process_backward)
+            backward_process.start()
+        elif dpg.is_item_active("right_button") and self.running_flag.value is False:
+            self.running_flag.value = True
+            forward_process = mp.Process(target=self.move_process_forward)
             forward_process.start()
-        # elif dpg.is_item_active("right_button") and self.running_flag.value is False:
-        #     self.running_flag.value = True
-        #     backward_process = mp.Process(target=self.motor.move_monochrom_forward_continuous)
-        #     backward_process.start()
 
-    def move_process(self):
+    def move_process_forward(self):
+        motor = Motor(self.running_flag)
+        motor.move_monochrom_forward_continuous()
+
+    def move_process_backward(self):
         motor = Motor(self.running_flag)
         motor.move_monochrom_backward_continuous()
 
