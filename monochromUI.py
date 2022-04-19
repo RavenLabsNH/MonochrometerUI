5e477974809666aca1d8f056b2ba91e31adf27c3
Jeffrey Nelson <jnelson@sunriselabs.com>
2022-04-19 14:55:19 -0400

    try new render method

--- monochromUI.py
+++ monochromUI.py
@@ -8,15 +8,16 @@ class MonochromUI():
         dpg.create_context()
         self.running_flag = mp.Value(c_bool, False)
         self.motor = Motor(self.running_flag)
+        self.index = 0
 
     def create_page(self):
         with dpg.handler_registry():
-            #dpg.add_mouse_down_handler(callback=self.move_monochrom)
+            dpg.add_mouse_down_handler(callback=self.move_monochrom)
             dpg.add_mouse_release_handler(callback=self.stop_mnonochrom)
 
         with dpg.window(tag="Monochrom", width=800, height=400) as window:
-            dpg.add_button(label="<", width=202, height=40, tag="left_button", callback=self.move_monochrom_left)
-            dpg.add_button(label=">", width=202, height=40, tag="right_button", callback=self.move_monochrom_right)
+            dpg.add_button(label="<", width=202, height=40, tag="left_button")
+            dpg.add_button(label=">", width=202, height=40, tag="right_button")
 
         dpg.create_viewport(title='Monochrom', width=800, height=400, x_pos=40, y_pos=40)
         dpg.setup_dearpygui()
@@ -33,16 +34,16 @@ class MonochromUI():
         Run the main DearPyGui render thread
         """
         while dpg.is_dearpygui_running():
-            dpg.render_dearpygui_frame()
+            self.index = self.index+1
+            if self.index%4 == 0:
+                dpg.render_dearpygui_frame()
 
-    def move_monochrom_left(self):
-        if self.running_flag.value is False:
+    def move_monochrom(self):
+        if dpg.is_item_active("left_button") and self.running_flag.value is False:
             self.running_flag.value = True
             forward_process = mp.Process(target=self.motor.move_monochrom_backward_continuous)
             forward_process.start()
-
-    def move_monochrom_right(self):
-        if self.running_flag.value is False:
+        elif dpg.is_item_active("right_button") and self.running_flag.value is False:
             self.running_flag.value = True
             backward_process = mp.Process(target=self.motor.move_monochrom_forward_continuous)
             backward_process.start()
