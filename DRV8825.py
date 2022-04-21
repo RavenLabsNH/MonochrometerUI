abd228f759d2e8695724ae23f4b5f5e5b56fe935
Jeffrey Nelson <jnelson@sunriselabs.com>
2022-04-21 15:06:04 -0400

    test limit switches

--- DRV8825.py
+++ DRV8825.py
@@ -98,9 +98,11 @@ class DRV8825():
         while self.running_flag.value == True:
             if (dir == MotorDir[0]) and GPIO.input(HIGH_PIN) == 0:
                 self.running_flag.value = False
+                print("Low triggerd")
                 break
             if (dir == MotorDir[1]) and GPIO.input(LOW_PIN) == 0:
                 self.running_flag.value = False
+                print("High triggerd")
                 break
             self.digital_write(self.step_pin, True)
             time.sleep(stepdelay)
